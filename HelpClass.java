808c3ed298830ce77dc2de1008f12bab28fe94a1
shadyfiiish <159940441+shadyfiiish@users.noreply.github.com>
2024-05-08 18:01:02 -0400

    Add files via upload

---
+++ HelpClass.java
@@ -0,0 +1,5 @@
+public class HelpClass {
+    public static void about() {
+        System.out.println("This software can help you find restaurants in Frederick based on the style of food that you want and the minimum ratings.");
+    }
+}
